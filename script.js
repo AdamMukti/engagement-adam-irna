bd13a9b617351a83c92e126e42c6a37704c4ac9f
Adam <adammukti08@gmail.com>
2023-08-02 23:20:59 +0700

    Add adam-irna and countdown

--- script.js
+++ script.js
@@ -11,7 +11,29 @@ const btnAudio = document.getElementById("btn-audio");
 const audio = document.getElementById("audio");
 const url_string = document.URL;
 const url = new URL(url_string);
+const timer = document.getElementById('timer');
+const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+let countDown = new Date('2023-08-13T00:00:00').getTime(),
+    x = setInterval(function () {
+        let now = new Date().getTime(),
+            distance = countDown - now;
+        const d = Math.floor(distance / (day));
+        const h = Math.floor(distance % (day) / (hour));
+        const m = Math.floor((distance % (hour)) / (minute));
+        const s = Math.floor((distance % (minute)) / second);
+        document.getElementById('days').innerText = d < 10 ? '0' + d : d;
+        document.getElementById('hours').innerText = h < 10 ? '0' + h : h;
+        document.getElementById('minutes').innerText = m < 10 ? '0' + m : m;
+        document.getElementById('seconds').innerText = s < 10 ? '0' + s : s;
 
+        if (distance < 0) {
+            clearInterval(x);
+        }
+
+    }, second);
 let namaUndangan;
 
 if (url.searchParams.get('to') != null) {
@@ -21,19 +43,17 @@ if (url.searchParams.get('to') != null) {
 }
 
 nama.innerHTML = namaUndangan;
-copyright.style.color = 'blue';
 
 open.addEventListener('click', function () {
     audio.play();
-    sectionSatu.classList.add('animate__slideOutUp')
     sectionSatu.classList.add('d-none')
     sectionDua.classList.add('animate__slideInUp')
     sectionDua.classList.remove('d-none')
     floatingButtonContainer.classList.remove('d-none')
-    content.style.backgroundImage = `url(bg_flower.jpg)`;
-    document.body.style.color = '#354259';
-    copyright.classList.remove('text-white');
-    copyright.style.color = 'blue';
+    // content.style.backgroundImage = `url(bg_flower.jpg)`;
+    // document.body.style.color = '#354259';
+    // copyright.classList.remove('text-white');
+    // copyright.style.color = 'blue';
 });
 
 btnAudio.addEventListener("click", function () {
@@ -49,3 +69,4 @@ btnAudio.addEventListener("click", function () {
 let footer = document.getElementById("credit");
 footer.innerHTML = "Adam Mukti";
 footer.href = "https://www.instagram.com/adamukti/";
+
